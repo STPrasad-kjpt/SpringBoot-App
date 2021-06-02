14c2f73e6c8d9f34098ed59f29a9cf7225529529
Thiru <stprasadmtech@gmail.com>
2021-06-02 18:31:21 +0530

    Main class modified

--- src/main/java/com/work/thirumala/SpringBootApp1Application.java
+++ src/main/java/com/work/thirumala/SpringBootApp1Application.java
@@ -2,12 +2,14 @@ package com.work.thirumala;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBootApp1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootApp1Application.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(SpringBootApp1Application.class, args);
+		context.close();
 	}
 
 }
