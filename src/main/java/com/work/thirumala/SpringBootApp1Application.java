f2445594b93f43598431e1f99c94ae45f42ffdb0
Thiru <stprasadmtech@gmail.com>
2021-06-02 21:28:15 +0530

    Feature-301 changes

--- src/main/java/com/work/thirumala/SpringBootApp1Application.java
+++ src/main/java/com/work/thirumala/SpringBootApp1Application.java
@@ -6,12 +6,17 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBootApp1Application {
+	private Map<String,Object> cache = new HashMap<String,Object>();
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(SpringBootApp1Application.class, args);
 		//changes for HIS-134
 		int a = 10;
 		context.close();
+	}
+	
+	public void loadToCache(){
+		//logic
 	}
 
 }
